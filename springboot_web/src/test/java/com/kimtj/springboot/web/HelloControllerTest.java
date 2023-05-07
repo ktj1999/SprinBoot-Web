81e51fe6dbd0c945e9a181c39dc741efda48527d
KIM <kimtj1999@naver.com>
2023-05-07 16:14:22 +0900

    테스트 코드 성공

--- springboot_web/src/test/java/com/kimtj/springboot/web/HelloControllerTest.java
+++ springboot_web/src/test/java/com/kimtj/springboot/web/HelloControllerTest.java
@@ -1,13 +1,12 @@
 package com.kimtj.springboot.web;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -15,16 +14,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
-public class HelloControllerTest extends TestCase {
+public class HelloControllerTest {
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
-    @WithMockUser(roles="USER")
     @Test
-    public void hello가_리턴된다() throws Exception {
-        String hello = "hello";
+    public void hello가_리턴된다()throws Exception{
+        String hello="hello";
 
-        mvc.perform(get("/hello"))
+        mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
